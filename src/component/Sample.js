6ad410ddfb96cb06fdba44cf32c05823a9910a88
Eric Atento <eric.atento@boom.camp>
2020-03-02 16:36:45 +0800

    update

--- src/component/Sample.js
+++ src/component/Sample.js
@@ -1,17 +1,57 @@
 import React, { useContext } from "react";
 import { store } from "../store";
+import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 
 export default function Sample() {
+  const classes = useStyles();
   const globalState = useContext(store);
   const { state, dispatch } = globalState;
 
   return (
     <>
-      <Button onClick={() => dispatch({ type: "changeColor" })}>
-        Click to Change to Blue
+      <CssBaseline />
+      <Container maxWidth="sm">
+        <Typography
+          component="div"
+          style={{
+            backgroundColor: `${state.backGroundColor}`,
+            height: "5vh",
+            borderRadius: 10,
+            marginBottom: 10
+          }}
+        />
+      </Container>
+
+      <Button
+        variant="contained"
+        color="primary"
+        style={{ marginRight: 10 }}
+        onClick={() => dispatch({ type: "changeColorBlue" })}
+      >
+        Change to Blue
       </Button>
-      <div>{console.log(state)}</div>
+      <Button
+        variant="contained"
+        color="secondary"
+        onClick={() => dispatch({ type: "changeColorRed" })}
+      >
+        Change to Red
+      </Button>
     </>
   );
 }
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary
+  }
+}));
--- src/reducer.js
+++ src/reducer.js
@@ -1,7 +1,9 @@
 export default function reducer(state, action) {
   switch (action.type) {
-    case "changeColor":
+    case "changeColorBlue":
       return { ...state, backGroundColor: "blue" };
+    case "changeColorRed":
+      return { ...state, backGroundColor: "Red" };
     default:
       throw new Error();
   }
